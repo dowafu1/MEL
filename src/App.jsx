f0f361fa7c2d723aeb2b33ebc60f01a4a55333b6
Lim Vitaly <yifanset2602@gmail.com>
2025-04-17 12:14:22 +0700

    complited api front to back

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css';
-// import PageHeader from './components/page/PageHeader';
+import PageHeader from './components/page/PageHeader';
+import MainBlock from './components/blocks/main-block/MainBlock';
+// import TestsBlock from './components/blocks/tests-block/TestsBlock';
+
 import DownloadBlock from './components/blocks/download-block/DownloadBlock';
 import PictureBlock from './components/blocks/picture-block/PictureBlock';
 import TestBlock from './components/blocks/test-block/TestBlock';
@@ -7,7 +10,10 @@ import TestBlock from './components/blocks/test-block/TestBlock';
 function App() {
   return (
     <>
-      {/* <PageHeader/> */}
+      <PageHeader/>
+      <MainBlock/>
+      {/* <TestsBlock/> */}
+
       <DownloadBlock/>
       <PictureBlock/>
       <TestBlock/>
