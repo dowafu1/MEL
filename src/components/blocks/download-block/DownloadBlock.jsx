ec133554b45b2a6b3164b74148e5a190c843e941
Lim Vitaly <yifanset2602@gmail.com>
2025-04-17 14:43:36 +0700

    router

---
+++ src/components/blocks/download-block/DownloadBlock.jsx
@@ -0,0 +1,220 @@
+import './download-block.css';
+import downloadSvg from './downloadsvg.svg';
+import moredownloadSvg from './moredownloadsvg.svg';
+import { useState } from 'react';
+
+const DownloadBlock = () => {
+    const [leftFile, setLeftFile] = useState(null);
+    const [rightFiles, setRightFiles] = useState([]);
+    const [checkedState, setCheckedState] = useState({
+        checkbox1: false,
+        checkbox2: false,
+        checkbox3: false
+    });
+
+    const defectTypes = [
+        { id: 1, name: "Смазанная картинка" },
+        { id: 2, name: "Искаженные тексты" },
+        { id: 3, name: "Несоответствие цветов" }
+    ];
+    
+    const handleCheckboxChange = (event) => {
+      const { name, checked } = event.target;
+      setCheckedState(prev => ({
+        ...prev,
+        [name]: checked
+      }));
+    };
+
+    const handleLeftFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setLeftFile(file);
+        }
+    };
+
+    const handleRightFilesChange = (e) => {
+        const files = Array.from(e.target.files);
+        if (files.length > 20) {
+            alert('Максимальное количество файлов - 20');
+            return;
+        }
+        setRightFiles(files);
+    };
+    
+    const handleSubmit = async () => {
+        if (!leftFile || rightFiles.length === 0) {
+        alert('Пожалуйста, загрузите файлы в оба блока');
+        return;
+    }
+    const isAnyCheckboxChecked = Object.values(checkedState).some(Boolean);
+        if (!isAnyCheckboxChecked) {
+            alert('Пожалуйста, выберите хотя бы один тип дефекта.');
+            return;
+    }
+    
+    const selectedDefects = defectTypes
+        .filter((_, index) => checkedState[`checkbox${index + 1}`])
+        .map(defect => defect.id);
+    if (selectedDefects.length === 0) {
+        alert('Пожалуйста, выберите хотя бы один тип дефекта');
+        return;
+    }
+    const formData = new FormData();
+    formData.append('example', leftFile);
+    rightFiles.forEach((file) => {
+        formData.append(`filesToCheck`, file);
+    });
+    
+    // Добавляем выбранные типы дефектов
+    formData.append('defectTypes', JSON.stringify(selectedDefects));
+    try {
+        const response = await fetch('https://....', { // сюда апи 
+            method: 'POST',
+            body: formData,
+        });
+        if (!response.ok) {
+            throw new Error('Ошибка при отправке файлов');
+        }
+        const result = await response.json();
+        console.log('Файлы успешно отправлены:', result);
+        alert('Файлы успешно отправлены на проверку');
+    } catch (error) {
+        console.error('Ошибка:', error);
+        alert('Произошла ошибка при отправке файлов');
+    }
+};
+
+    return (
+        <div className='download-block__container'>
+            <div className="download-block__typedefects">
+                <p className="download-block__text">ТИПЫ ДЕФЕКТОВ</p>
+                <div className="download-block__typedefects-list">
+                    {defectTypes.map((defect, index) => (
+                      <div key={`checkbox${index + 1}`} className="download-block__checkbox-item">
+                        <input
+                          type="checkbox"
+                          id={`checkbox${index + 1}`}
+                          name={`checkbox${index + 1}`}
+                          className="download-block__checkbox-input"
+                          checked={checkedState[`checkbox${index + 1}`]}
+                          onChange={handleCheckboxChange}
+                        />
+                        <label htmlFor={`checkbox${index + 1}`} className="download-block__checkbox-label">
+                           {defect.name}
+                        </label>
+                      </div>
+                    ))}                   
+                </div>
+            </div>
+
+
+            <p className="download-block__text">ЗАГРУЗКА ФАЙЛОВ</p>
+            <div className="download-block__blocks">
+                <div className="download-block__left-block">
+                    <div className="download-block__block">
+                        <div className="download-block__text-overlay">
+                            ВАШ ЭТАЛОН
+                        </div>
+                        <div className="download-block__content">
+                            {leftFile ? (
+                                <div className="download-block__file-info">
+                                    <p>{leftFile.name}</p>
+                                    <p>{(leftFile.size / 1024).toFixed(2)} KB</p>
+                                </div>
+                            ) : (
+                                <>
+                                    <div className="download-block__download">
+                                        <img 
+                                            src={downloadSvg} 
+                                            alt="download" 
+                                            className="download-block__svg"
+                                        />
+                                        <div className="download-block__text1">
+                                            1 файл
+                                        </div>
+                                    </div>
+                                    
+                                </>
+                            )}
+                        </div>
+                    </div>
+                    <div className="">
+                        <input 
+                            type="file" 
+                            id="left-file-input" 
+                            style={{ display: 'none' }} 
+                            onChange={handleLeftFileChange}
+                            accept="*"
+                        />
+                        <button 
+                            className="download-block__btn"
+                            onClick={() => document.getElementById('left-file-input').click()}
+                        >
+                            {leftFile ? 'Файл выбран' : 'Выберите файл'}
+                        </button>
+                    </div>
+                </div>
+
+                <div className="download-block__right-block">
+                    <div className="download-block__block">
+                        <div className="download-block__text-overlay">
+                            ЧТО ПРОВЕРИТЬ
+                        </div>
+                        <div className="download-block__content">
+                            {rightFiles.length > 0 ? (
+                                <div className="download-block__files-list">
+                                    <p>Выбрано файлов: {rightFiles.length}/20</p>
+                                    {rightFiles.slice(0, 3).map((file) => (
+                                        <p key={file.name}>{file.name}</p>
+                                    ))}
+                                    {rightFiles.length > 3 && <p>...и еще {rightFiles.length - 3}</p>}
+                                </div>
+                            ) : (
+                                <>
+                                    <div className="download-block__download">
+                                        <img 
+                                            src={moredownloadSvg} 
+                                            alt="download" 
+                                            className="download-block__svg"
+                                        />
+                                        <div className="download-block__text1">
+                                            максимум<br/>20 файлов
+                                        </div>
+                                    </div>
+                                    
+                                </>
+                            )}
+                        </div>
+                    </div>
+                    <div className="">
+                        <input 
+                            type="file" 
+                            id="right-files-input" 
+                            style={{ display: 'none' }} 
+                            onChange={handleRightFilesChange}
+                            multiple
+                            accept="*"
+                        />
+                        <button 
+                            className="download-block__btn"
+                            onClick={() => document.getElementById('right-files-input').click()}
+                        >
+                            {rightFiles.length > 0 ? `Выбрано ${rightFiles.length} файлов` : 'Выберите файлы'}
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="download-block__submit-container">
+                <button 
+                    className="download-block__submit-btn"
+                    onClick={handleSubmit}
+                >
+                    Начать проверку
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default DownloadBlock;
---
+++ src/components/blocks/picture-block/PictureBlock.jsx
@@ -0,0 +1,349 @@
+import './picture-block.css';
+import saveSvg from './savesvg.svg';
+import manJpg from './man.jpg';
+import pencilSng from './pencil.svg';
+
+const PictureBlock = () => {
+    return (
+        <div className='picture-block__container'>
+            <div className="picture-block__title">
+                <p className="picture-block__text">ВАШИ ИЗОБРАЖЕНИЯ</p>
+                <div className="">
+                    <button className="picture-block__btn1">
+                        Сохранить все карточки
+                    </button>
+                </div>
+                <div className="">
+                    <img 
+                        src={saveSvg} 
+                        alt="download" 
+                        className="picture-block__svg"
+                    />
+                </div>
+            </div>
+
+            <div className="picture-block__blocks-list">
+                <div className="picture-block__blocks">
+                <div className="picture-block__left-block">
+                    <img 
+                        src={manJpg} 
+                        alt="download" 
+                        className="picture-block__img"
+                    />
+                </div>
+                <div className="picture-block__right-block">
+                    <p className="picture-block__name-file">
+                        Имя файла
+                    </p>
+                    <div className="picture-block__result">
+                        <p className="picture-block__result-text1">
+                            РЕЗУЛЬТАТ:
+                        </p>
+                        <div className="picture-block__results">
+                            <div className="picture-block__result1">
+                                <p className="picture-block__result-percent">
+                                    85,81%
+                                </p>
+                                <p className="picture-block__result-text2">
+                                    совпадение<br/>с примером
+                                </p>
+                            </div>
+                            <div className="picture-block__result2">
+                                <p className="picture-block__result-percent">
+                                    83,51%
+                                </p>
+                                <p className="picture-block__result-text2">
+                                    уверенность ИИ<br/>в своем вердикте
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="picture-block__defects">
+                        <p className="picture-block__defect-text">
+                            ДЕФЕКТЫ:
+                        </p>
+                        <div className="picture-block__defect-list">
+                            <div className="picture-block__defect">
+                                <div className="picture-block__defect1-part1">
+                                    <p className="picture-block__defect-text1">
+                                        Смазанная картинка
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                                
+                                <div className="picture-block__defect1-part2">
+                                    <p className="picture-block__defect-text1">
+                                        A область
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                            </div>
+                            <div className="picture-block__defect">
+                                <div className="picture-block__defect1-part1">
+                                    <p className="picture-block__defect-text1">
+                                        Искаженные тексты
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                                
+                                <div className="picture-block__defect1-part2">
+                                    <p className="picture-block__defect-text1">
+                                        В область
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="picture-block__btns">
+                        <div className="">
+                            <button className="picture-block__btn2">
+                                Внести изменения
+                            </button>
+                        </div>
+
+                        <div className="">
+                            <button className="picture-block__btn3">
+                                Сохранить карточку
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+                <div className="picture-block__blocks">
+                <div className="picture-block__left-block">
+                    <img 
+                        src={manJpg} 
+                        alt="download" 
+                        className="picture-block__img"
+                    />
+                </div>
+                <div className="picture-block__right-block">
+                    <p className="picture-block__name-file">
+                        Имя файла
+                    </p>
+                    <div className="picture-block__result">
+                        <p className="picture-block__result-text1">
+                            РЕЗУЛЬТАТ:
+                        </p>
+                        <div className="picture-block__results">
+                            <div className="picture-block__result1">
+                                <p className="picture-block__result-percent">
+                                    85,81%
+                                </p>
+                                <p className="picture-block__result-text2">
+                                    совпадение<br/>с примером
+                                </p>
+                            </div>
+                            <div className="picture-block__result2">
+                                <p className="picture-block__result-percent">
+                                    83,51%
+                                </p>
+                                <p className="picture-block__result-text2">
+                                    уверенность ИИ<br/>в своем вердикте
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="picture-block__defects">
+                        <p className="picture-block__defect-text">
+                            ДЕФЕКТЫ:
+                        </p>
+                        <div className="picture-block__defect-list">
+                            <div className="picture-block__defect">
+                                <div className="picture-block__defect1-part1">
+                                    <p className="picture-block__defect-text1">
+                                        Смазанная картинка
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                                
+                                <div className="picture-block__defect1-part2">
+                                    <p className="picture-block__defect-text1">
+                                        A область
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                            </div>
+                            <div className="picture-block__defect">
+                                <div className="picture-block__defect1-part1">
+                                    <p className="picture-block__defect-text1">
+                                        Искаженные тексты
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                                
+                                <div className="picture-block__defect1-part2">
+                                    <p className="picture-block__defect-text1">
+                                        В область
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="picture-block__btns">
+                        <div className="">
+                            <button className="picture-block__btn2">
+                                Внести изменения
+                            </button>
+                        </div>
+
+                        <div className="">
+                            <button className="picture-block__btn3">
+                                Сохранить карточку
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+                <div className="picture-block__blocks">
+                <div className="picture-block__left-block">
+                    <img 
+                        src={manJpg} 
+                        alt="download" 
+                        className="picture-block__img"
+                    />
+                </div>
+                <div className="picture-block__right-block">
+                    <p className="picture-block__name-file">
+                        Имя файла
+                    </p>
+                    <div className="picture-block__result">
+                        <p className="picture-block__result-text1">
+                            РЕЗУЛЬТАТ:
+                        </p>
+                        <div className="picture-block__results">
+                            <div className="picture-block__result1">
+                                <p className="picture-block__result-percent">
+                                    85,81%
+                                </p>
+                                <p className="picture-block__result-text2">
+                                    совпадение<br/>с примером
+                                </p>
+                            </div>
+                            <div className="picture-block__result2">
+                                <p className="picture-block__result-percent">
+                                    83,51%
+                                </p>
+                                <p className="picture-block__result-text2">
+                                    уверенность ИИ<br/>в своем вердикте
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="picture-block__defects">
+                        <p className="picture-block__defect-text">
+                            ДЕФЕКТЫ:
+                        </p>
+                        <div className="picture-block__defect-list">
+                            <div className="picture-block__defect">
+                                <div className="picture-block__defect1-part1">
+                                    <p className="picture-block__defect-text1">
+                                        Смазанная картинка
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                                
+                                <div className="picture-block__defect1-part2">
+                                    <p className="picture-block__defect-text1">
+                                        A область
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                            </div>
+                            <div className="picture-block__defect">
+                                <div className="picture-block__defect1-part1">
+                                    <p className="picture-block__defect-text1">
+                                        Искаженные тексты
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                                
+                                <div className="picture-block__defect1-part2">
+                                    <p className="picture-block__defect-text1">
+                                        В область
+                                    </p>
+                                    <img 
+                                        src={pencilSng} 
+                                        alt="pencil" 
+                                        className="picture-block__svg1"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="picture-block__btns">
+                        <div className="">
+                            <button className="picture-block__btn2">
+                                Внести изменения
+                            </button>
+                        </div>
+
+                        <div className="">
+                            <button className="picture-block__btn3">
+                                Сохранить карточку
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+      
+        </div>
+    )
+}
+
+export default PictureBlock;
